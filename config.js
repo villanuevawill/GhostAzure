230dd036905e0f7335d6dda6a52c1ae380d38161
William Villanueva <wvillanu89@gmail.com>
2014-06-15 22:29:00 -0700

    Configure Ghost for deployment to Azure in a production environment

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        url: 'http://willvillanueva.azurewebsites.net/',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
