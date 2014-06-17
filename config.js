b2ac5c81c7c355418493c59719858deef37ec7ca
William Villanueva <wvillanu89@gmail.com>
2014-06-17 13:18:42 -0700

    Changed root URL

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.willvillanueva.com',
         mail: {},
         database: {
             client: 'sqlite3',
